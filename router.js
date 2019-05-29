0cdf69dcb4548617f6ad20f32d7aea81e6b0f60f
Caaalabash <1121062986@qq.com>
2019-05-29 22:42:53 +0800

    feat: child_process timeout

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 const router = require('koa-router')()
 const path = require('path')
-const { writeFile, execCommand, getFilename } = require('./util')
+const { writeFile, execCommand, getUniqueFilename } = require('./util')
 
 const hostPath = path.join(__dirname, '/code')
 const workPath = '/code'
-let count = 0
+let idx = 0
 
 router.get('/', async ctx => {
   await ctx.render('index', {
@@ -15,8 +15,9 @@ router.get('/', async ctx => {
   })
 })
 router.post('/runner', async (ctx) => {
+  idx += 1
   const { language, code, version } = ctx.request.body
-  const filename = getFilename(language)
+  const filename = getUniqueFilename(language, idx)
   const filePath = path.join(hostPath, filename)
 
   const [writeErr, ] = await writeFile(hostPath, filename, code)
@@ -28,10 +29,11 @@ router.post('/runner', async (ctx) => {
       data: writeErr || execErr
     }
   }
-
-  const [e, data] = await execCommand(`
-    docker run --rm --name runner${count++} -v ${hostPath}:${workPath} ${language}:${version} ${language} ${filePath}
-  `, { timeout: 10000 })
+  const [e, data] = await execCommand(
+    `docker run --rm --memory=50m --name runner-${idx} -v ${hostPath}:${workPath} ${language}:${version} ${language} ${filePath}`,
+    { timeout: 10000 },
+    `docker stop runner-${idx}`
+  )
 
   return ctx.body = {
     errno: 0,
--- util/index.js
+++ util/index.js
@@ -3,14 +3,32 @@ const fs = require('fs')
 const exec = require('child_process').exec
 
 module.exports = {
-  execCommand(command, options) {
+  /**
+   * @param {string} command 要运行的命令
+   * @param {object} options 选项
+   * @param {string} timeoutCommand 代码执行超时时执行的命令
+   */
+  execCommand(command, options, timeoutCommand) {
     return new Promise((resolve, reject) => {
-      exec(command, options, (e, data) => {
+      const child_process = exec(command, options, (e, stdout, stderr) => {
         if (e) reject(e)
+        else if (stderr) reject(stderr)
         else resolve(data)
+      })
+      child_process.on('exit', (code, signal) => {
+        if (signal) {
+          exec(timeoutCommand, { timeout: 5000 }, () => {
+            reject(new Error('代码执行超时'))
+          })
+        }
       })
     }).then(data => [null, data]).catch(err => [err.message, null])
   },
+  /**
+   * @param {string} folder 目标文件夹
+   * @param {string} filename 文件名
+   * @param {object} data 文件数据
+   */
   writeFile(folder, filename, data) {
     return new Promise((resolve, reject) => {
       fs.writeFile(path.join(folder, filename), data, e => {
@@ -32,12 +50,17 @@ module.exports = {
       })
     }).then(data => [null, data]).catch(err => [err, null])
   },
-  getFilename(language) {
+  /**
+   * @param {string} language 语言
+   * @param {number} idx 唯一索引
+   * @return {string} 唯一文件名
+   */
+  getUniqueFilename(language, idx) {
     switch (language) {
       case 'python':
-        return 'main.py'
+        return `main-${idx}.py`
       default:
-        return 'man.js'
+        return `main-${idx}.js`
     }
   }
 }
